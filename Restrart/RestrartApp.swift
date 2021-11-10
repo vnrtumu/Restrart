05d973930800a717b560fe8b666c7caca73ebc7e
venkat reddy <vnr.tumu@gmail.com>
2021-11-10 18:56:19 +0530

    Initial Commit

---
+++ Restrart/RestrartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestrartApp.swift
+//  Restrart
+//
+//  Created by venkat reddy on 10/11/21.
+//
+
+import SwiftUI
+
+@main
+struct RestrartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
